f7f467d14da26ee334d8da4f9dc98afb4eee3462
Manuel Meurer <manuel@krautcomputing.com>
2014-10-22 15:16:23 +0200

    log when rescheduling service

--- lib/services/modules/uniqueness_checker.rb
+++ lib/services/modules/uniqueness_checker.rb
@@ -82,6 +82,7 @@ module Services
         reschedule_args = @service_args.map do |arg|
           convert_for_rescheduling arg
         end
+        log "Rescheduling to be executed in #{retry_delay} seconds." if self.respond_to?(:log)
         self.class.perform_in retry_delay, *reschedule_args
       end
 
