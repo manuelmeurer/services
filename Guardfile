3621363776849fad2598364cde3ff59e1e873fcb
Manuel Meurer <manuel@krautcomputing.com>
2014-11-21 14:54:19 +0100

    move redis stuff from spec helper to guardfile

--- Guardfile
+++ Guardfile
@@ -1,4 +1,62 @@
+require 'guard/rspec'
+
+module ::Guard
+  class RSpec < Plugin
+    # Add `stop` method if not defined
+    # so that `stop_*` callbacks work.
+    unless instance_methods.include?(:stop)
+      def stop; end
+    end
+  end
+
+  module ServicesGemHelpers
+    SPEC_SUPPORT_DIR = Pathname.new(File.expand_path('../spec/support', __FILE__))
+    REDIS_BIN        = SPEC_SUPPORT_DIR.join('redis-server')
+    REDIS_CLI        = SPEC_SUPPORT_DIR.join('redis-cli')
+    REDIS_PIDFILE    = SPEC_SUPPORT_DIR.join('redis.pid')
+    REDIS_LOGFILE    = SPEC_SUPPORT_DIR.join('log', 'redis.log')
+
+    class OnStart
+      def call(guard_class, event, *args)
+        redis_options = {
+          daemonize:  'yes',
+          dir:        SPEC_SUPPORT_DIR,
+          dbfilename: 'redis.rdb',
+          logfile:    REDIS_LOGFILE,
+          pidfile:    REDIS_PIDFILE,
+        }
+        system "#{REDIS_BIN} #{redis_options.map { |k, v| "--#{k} #{v}" }.join(' ')}"
+
+        i = 0
+        while !File.exist?(REDIS_PIDFILE)
+          puts 'Waiting for Redis to start...'
+          sleep 1
+          i += 1
+          raise "Redis didn't start in #{i} seconds." if i >= 5
+        end
+      end
+    end
+
+    class OnStop
+      def call(guard_class, event, *args)
+        system "#{REDIS_CLI} shutdown"
+
+        i = 0
+        while File.exist?(REDIS_PIDFILE)
+          puts 'Waiting for Redis to stop...'
+          sleep 1
+          i += 1
+          raise "Redis didn't stop in #{i} seconds." if i >= 5
+        end
+      end
+    end
+  end
+end
+
 guard 'rspec', cmd: 'bundle exec rspec' do
+  callback ServicesGemHelpers::OnStart.new, :start_begin
+  callback ServicesGemHelpers::OnStop.new,  :stop_begin
+
   # Specs
   watch(%r(^spec/.+_spec\.rb$))
   watch('spec/spec_helper.rb')       { 'spec' }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,6 @@ require_relative '../lib/services'
 PROJECT_ROOT       = Pathname.new(File.expand_path('../..', __FILE__))
 TEST_SERVICES_PATH = Pathname.new(File.join('spec', 'support', 'test_services.rb'))
 support_dir        = Pathname.new(File.expand_path('../support', __FILE__))
-log_dir            = support_dir.join('log')
 
 CALL_PROXY_SOURCE      = support_dir.join('call_proxy.rb')
 CALL_PROXY_DESTINATION = PROJECT_ROOT.join('lib', 'services', 'call_proxy.rb')
@@ -20,23 +19,19 @@ STOP_TIMEOUT           = 20
 
 Dir[support_dir.join('**', '*.rb')].each { |f| require f }
 
-redis_port    = 6379
-redis_pidfile = support_dir.join('redis.pid')
-redis_url     = "redis://localhost:#{redis_port}/0"
-
 sidekiq_pidfile = support_dir.join('sidekiq.pid')
 sidekiq_timeout = 20
 
 Services.configure do |config|
-  config.redis  = Redis.new
+  config.redis = Redis.new
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { redis: redis_url, namespace: 'sidekiq', size: 1 }
+  config.redis = { redis: 'redis://localhost:6379/0', namespace: 'sidekiq', size: 1 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { redis: redis_url, namespace: 'sidekiq' }
+  config.redis = { redis: 'redis://localhost:6379/0', namespace: 'sidekiq' }
 end
 
 RSpec.configure do |config|
@@ -45,20 +40,6 @@ RSpec.configure do |config|
   config.order = 'random'
 
   config.before :suite do
-    unless ENV['TRAVIS']
-      # Start Redis
-      redis_options = {
-        daemonize:  'yes',
-        port:       redis_port,
-        dir:        support_dir,
-        dbfilename: 'redis.rdb',
-        logfile:    log_dir.join('redis.log'),
-        pidfile:    redis_pidfile
-      }
-      redis = support_dir.join('redis-server')
-      system "#{redis} #{options_hash_to_string(redis_options)}"
-    end
-
     # Start Sidekiq
     sidekiq_options = {
       concurrency: 10,
@@ -66,7 +47,7 @@ RSpec.configure do |config|
       timeout:     sidekiq_timeout,
       verbose:     true,
       require:     __FILE__,
-      logfile:     log_dir.join('sidekiq.log'),
+      logfile:     support_dir.join('log', 'sidekiq.log'),
       pidfile:     sidekiq_pidfile
     }
     system "bundle exec sidekiq #{options_hash_to_string(sidekiq_options)}"
@@ -74,49 +55,29 @@ RSpec.configure do |config|
     # Copy call proxy
     FileUtils.cp CALL_PROXY_SOURCE, CALL_PROXY_DESTINATION
 
-    # Wait until Redis and Sidekiq are started
-    @processes = {
-      'Sidekiq' => sidekiq_pidfile
-    }
-    @processes['Redis'] = redis_pidfile unless ENV['TRAVIS']
-    @processes.each do |process, pidfile|
-      i = 0
-      while !File.exist?(pidfile)
-        puts "Waiting for #{process} to start..."
-        sleep WAIT
-        i += WAIT
-        raise "#{process} didn't start in #{i} seconds." if i >= START_TIMEOUT
-      end
+    # Wait for Sidekiq to start
+    i = 0
+    while !File.exist?(sidekiq_pidfile)
+      puts 'Waiting for Sidekiq to start...'
+      sleep WAIT
+      i += WAIT
+      raise "Sidekiq didn't start in #{i} seconds." if i >= START_TIMEOUT
     end
   end
 
   config.after :suite do
-    # Delete call proxy
-    FileUtils.rm CALL_PROXY_DESTINATION
-
     # Stop Sidekiq
     system "bundle exec sidekiqctl stop #{sidekiq_pidfile} #{sidekiq_timeout}"
 
-    unless ENV['TRAVIS']
-      # Stop Redis
-      redis_cli = support_dir.join('redis-cli')
-      system "#{redis_cli} -p #{redis_port} shutdown"
-    end
+    # Delete call proxy
+    FileUtils.rm CALL_PROXY_DESTINATION
 
-    @processes.each do |process, pidfile|
-      i = 0
-      while File.exist?(pidfile)
-        puts "Waiting for #{process} to stop..."
-        sleep WAIT
-        i += WAIT
-        raise "#{process} didn't stop in #{i} seconds." if i >= STOP_TIMEOUT
-      end
-    end
-
-    # Truncate log files
-    max_len = 1024 * 1024 # 1 MB
-    Dir[File.join(log_dir, '*.log')].each do |file|
-      File.truncate file, max_len if File.size(file) > max_len
+    i = 0
+    while File.exist?(sidekiq_pidfile)
+      puts 'Waiting for Sidekiq to stop...'
+      sleep WAIT
+      i += WAIT
+      raise "Sidekiq didn't stop in #{i} seconds." if i >= STOP_TIMEOUT
     end
   end
 
