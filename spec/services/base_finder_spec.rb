c6b9f3e69e584f162a247d2ec8ed2daacfd1a294
Manuel Meurer <manuel@krautcomputing.com>
2014-11-25 15:13:33 +0100

    use `let` in specs

--- spec/services/base_finder_spec.rb
+++ spec/services/base_finder_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 describe Services::BaseFinder do
   include_context 'capture logs'
 
+  let(:base_find) { Services::Models::BaseFind }
+
   it 'has call logging disabled by default' do
-    expect(Services::Models::BaseFind.call_logging_disabled).to eq(true)
-    expect { Services::Models::BaseFind.call }.to_not change { logs }
+    expect(base_find.call_logging_disabled).to eq(true)
+    expect { base_find.call }.to_not change { logs }
   end
 end
