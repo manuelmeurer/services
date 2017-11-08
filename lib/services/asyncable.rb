261bdf7ac34a4bc8049a3ad1669c0a84dcac5069
Manuel Meurer <manuel@krautcomputing.com>
2017-11-08 12:16:32 +0100

    remove bulk call async method

--- lib/services/asyncable.rb
+++ lib/services/asyncable.rb
@@ -18,15 +18,6 @@ module Services
     end
 
     module ClassMethods
-      # Bulk enqueue items
-      # args can either be a one-dimensional or two-dimensional array,
-      # each item in args should be the arguments for one job.
-      def bulk_call_async(args)
-        # Convert args to two-dimensional array if it isn't one already.
-        args = args.map { |arg| [arg] } if args.none? { |arg| arg.is_a?(Array) }
-        Sidekiq::Client.push_bulk 'class' => self, 'args' => args
-      end
-
       ASYNC_METHOD_SUFFIXES.each do |async_method_suffix|
         define_method "call_#{async_method_suffix}" do |*args|
           self.public_send "perform_#{async_method_suffix}", *args
